863c55887226abc69de5a16a7e921706b504072e
Samantha <123sjstevens@gmail.com>
2024-09-19 20:07:44 +1000

    bug fix with textbox

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0, 0, -10);
+    Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+
+        //smooth movement
+        //Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        //Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
+
+        transform.position = desiredPos;
+        //transform.LookAt(target);
+    }
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
-{
-    public float movementSpeed = 1;
+{ 
+    public float movementSpeed = 4;
     public string charName;
     public int canMove = 1;
+    public float smoothTime = 0.125f;
+    public Vector2 velocity;
+
+    Rigidbody2D rb;
 
     private void OnEnable()
     {
@@ -15,7 +19,7 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,7 +27,13 @@ public class CharacterMovement : MonoBehaviour
     {
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * movementSpeed * canMove;
+        Vector3 moveDir = new Vector3(horizontal, vertical, 0);
+        Vector3 targetVelocity = moveDir * movementSpeed;
+
+        //Vector3 smoothed = Vector2.SmoothDamp(transform.position, targetVelocity, ref velocity, smoothTime);
+        //transform.position = smoothed;
+
+        rb.velocity = targetVelocity * canMove;
     }
 
     void GetName(string name)
